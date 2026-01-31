1316e8e4d03e21a078f9945bafe03e256c3f141e
Mayank Gaur <mayankgaur13004@gmail.com>
2026-02-01 02:08:42 +0530

    Solved 1845A

--- Day 11-20/cf1845A.py
+++ Day 11-20/cf1845A.py
@@ -1,11 +1,30 @@
 for _ in range(int(input())):
     n,k,x=map(int,input().split())
     
-    if x==1 and k<2:
-        print('NO')
-    elif n%2!=0 and x==1:
-        print('NO')
+    if k>1:
+        if x!=1:
+            print('YES')
+            print(n)
+            print(n*'1 ')
+            
+        elif x==1:
+            if n%2==0:
+                print('YES')
+                print(n//2)
+                print((n//2)*'2 ')
+            else:
+                if k>2:
+                    print('YES')
+                    print(n//2)
+                    print(((n//2)-1)*'2 '+'3')
+                
+                else:
+                    print('NO')
     else:
-        print('YES')
-        print(n)
-        print('1 '*n)
+        if k==1:
+            if x==1:
+                print('NO')
+            else:
+                print('YES')
+                print(n)
+                print(n*'1 ')
