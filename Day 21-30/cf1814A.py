150d83e8442082b8cdf9d6588764263592a517fb
Mayank Gaur <mayankgaur13004@gmail.com>
2026-02-08 21:03:31 +0530

    solved 1814A

--- Day 21-30/cf1814A.py
+++ Day 21-30/cf1814A.py
@@ -1,17 +1,21 @@
 for _ in range(int(input())):
     n,k=map(int,input().split())
-    if k==1:
+    if n%2==0 or (n-k)%2==0:
         print('YES')
-    else:        
-        while n%k==0:
-            n//=k
+    else:
+        print('NO')
+    # if k==1:
+    #     print('YES')
+    # else:        
+    #     while n%k==0:
+    #         n//=k
 
-        print(n)
-        # while n%2==0:
-        #     n//=2
-        # print(n)
+    #     print(n)
+    #     # while n%2==0:
+    #     #     n//=2
+    #     # print(n)
 
-        if n%2==0:
-            print('YES')
-        else:
-            print('NO')
+    #     if n%2==0:
+    #         print('YES')
+    #     else:
+    #         print('NO')
